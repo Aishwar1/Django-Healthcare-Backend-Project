adb47300bb6899fbedd71befdeecc43279265901
Aishwar <aishwar.16218@gmail.com>
2026-05-14 20:26:40 +0530

    HAHHAH

---
+++ healthcare_backend/patients/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Patient(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    disease = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
